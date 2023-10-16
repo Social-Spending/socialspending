3bebe1088d725cf1bf66e2364160b170ccf24637
Ryder Reed <ryderreed15@gmail.com>
2023-10-16 14:52:19 -0400

    Added functional signup.php
    Added a temporary signup.php to be used only for test. Should be replaced with better code in the future

--- signup.php
+++ signup.php
@@ -1,25 +1,81 @@
 <?php
-    // Must do cookie setting here, before any content is sent
-    // if the user's current cookie corresponds to an account, log them in
+
+
+
+include_once('templates/connection.php');
+
+
+
+
+
+
+
+// get data from POST
+// TODO: protect against SQL injection
+$user 			= $_POST['user'];
+$email 			= $_POST['email'];
+$password 	= $_POST['password'];
+
+// lookup user by username or by email to check if one already exists
+// SQL warns for this but can be mitigated by turning off index warnings or indexing the table which likely wont lead to any performance improvements
+$result = $mysqli->query('SELECT * FROM `users` WHERE `username`=\'' . $user . '\' OR `email`=\'' . $email . '\';');
+// check that query was successful
+if (!$result)
+{
+    // query failed, internal server error
+    print('Unable to contact database');
+    http_response_code(500);
+    exit(0);
+}
+
+// check that user was found
+if ($result->num_rows === 0)
+{
+    
+	
+		// get max uid so far
+		$result 	= $mysqli->query('SELECT MAX(user_id) as max_uid FROM users');
+		
+		if (!$result)
+		{
+			// query failed, internal server error
+			print('Unable to contact database');
+			http_response_code(500);
+			exit(0);
+		}
+		
+		$row = mysqli_fetch_array($result);
+		
+        // generate and associate this UID and password hash
+		$uid = $row["max_uid"] + 1;
+		$passwordHash = password_hash($password,  PASSWORD_BCRYPT);
+		
+		//insert new user into database
+		$result 	= $mysqli->query('INSERT INTO users (user_id, email, username, pass_hash) VALUES (\'' . $uid . '\', \'' . $email . '\', \'' . $user . '\', \'' . $passwordHash . '\');');
+								
+							
+        // check status of query
+        if (!$result)
+        {
+            // query failed, internal server error
+            print('Unable to contact database');
+            http_response_code(500);
+            exit(0);
+        }
+        // return okay status code
+		
+		
+        http_response_code(200);
+        exit(0);
+    
+}
+else
+{
+    // user not found
+    print('User already exists');
+}
+
+// authentication failed, return status code 403 and print error message
+http_response_code(403);
+exit(0);
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <script src="unauthenticated_header.js"></script>
-        <script src="signup.js"></script>
-        <link rel="stylesheet" href="global.css"/>
-        <link rel="stylesheet" href="signup.css"/>
-        <link rel="stylesheet" href="header.css"/>
-    </head>
-
-    <body>
-        <?php readfile('templates/unauthenticated_header.html'); ?>
-        <div class="container">
-            <div id="signupForm">
-                <label for="signupForm_username">Username</label>
-                <input type="text" id="signupForm_username">
-            </div>
-        </div>
-    </body>
-</html>
