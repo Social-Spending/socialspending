8e35edec9032925e83e26041e955b2ccdaaa21db
francesmatthew <frances.matthew.c@gmail.com>
2023-10-16 11:07:52 -0400

    index redirect to login

--- react_source/app/index.js
+++ react_source/app/index.js
@@ -1,13 +1,13 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Redirect } from "expo-router";
 
 
 export default function Page() {
-	
-	
+
+
   return (
-		<Text>Test</Text>
+    <Redirect href='/login'/>
   );
 }
 
