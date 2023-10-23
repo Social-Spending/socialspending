928bb5e6dc170f0511f3784a889dbaeba40eeaf5
Ryder Reed <ryderreed15@gmail.com>
2023-10-23 16:22:18 -0400

    Created a template for pending transactions

--- react_source/components/Notifications.js
+++ react_source/components/Notifications.js
@@ -1,38 +1,111 @@
 import * as globals from '../utils/globals.js'
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useState } from 'react';
+import Button from './Button.js';
 
-export default function Notifications() {
-	return (
-		<View>
-			<h1 style={styles.header}>Friend Requests</h1>
-			<p>Friend request from x</p>
-			<h1 style={styles.header}>Awaiting Approval</h1>
-			<p>Transaction name</p>
-			<button>View</button>
-			<button>Accept</button>
-			<button>Reject</button>
-			<h1 style={styles.header}>Completed Transactions</h1>
-			<p>Transaction name</p>
-			<button>View</button>
-		</View>
-	);
+import ApproveSvg   from '../assets/images/bx-check.svg';
+import DenySvg      from '../assets/images/bx-x.svg';
+import DetailsSvg   from '../assets/images/bx-detail.svg';
+
+export default function Notifications(props) {
+    return (
+        <View style={[styles.notifShelf, props.show ? { width: '20vw', borderLeftStyle: 'solid' } : { width: '0vh' }]}>
+            <View style={[props.show ? { width: '18vw', display: "block" } : { width: '0', display: "none"}]}>
+                <h1 style={styles.header}>Friend Requests</h1>
+                <p>Friend request from x</p>
+                <Text style={[globals.styles.h2, { paddingLeft: 0, color: globals.COLOR_GRAY }]}>Pending Transactions</Text>
+                <ApproveTransaction name="Transaction One" date={"1/2/23"}></ApproveTransaction>
+                <ApproveTransaction name="Transaction Two Hopefully this is long enough to overflow" date={"3/2/23"}></ApproveTransaction>
+                <ApproveTransaction name="Transaction Three Hopefully this is long enough to overflow im going to make it even longer to show more text" date={"6/2/23"}></ApproveTransaction>
+                <h1 style={styles.header}>Completed Transactions</h1>
+                <p>Transaction name</p>
+                <button>View</button>
+            </View>
+            
+        </View>
+    );
 }
 
-let button_style = globals.styles.formButton;
+function FriendRequest() {
+
+}
+
+function ApproveTransaction(props) {
+
+    return (
+        <View style={styles.notification}>
+            
+            <View style={{flex: 1}}>
+                <Text style={styles.text}>New Pending Transaction {props.date}</Text>
+                <View style={{ flexDirection: 'columnn', justifyContent: 'center' }}>
+                    <Text style={styles.notificationText}>{props.name}</Text>
+                </View>
+                
+            </View>
+            <View style={styles.buttonContainer}>
+
+                <Button style={[styles.button, { backgroundColor: globals.COLOR_WHITE }]} svg={DetailsSvg} iconStyle={{ fill: globals.COLOR_BLACK }} />
+                <Button style={[styles.button, { backgroundColor: globals.COLOR_WHITE }]} svg={ApproveSvg} iconStyle={{ fill: globals.COLOR_BLUE, width: '2em' }} />
+                <Button style={[styles.button, { backgroundColor: globals.COLOR_WHITE }]} svg={DenySvg} iconStyle={{ fill: globals.COLOR_ORANGE, width: '2em' }} />
+            </View>
+                      
+        </View>
+        
+    );
+}
+
+
 
 const styles = StyleSheet.create({
-	header: {
-        padding: '0.2em',
-        fontSize: '2em',
-        fontWeight: 'bolder',
-		color: globals.COLOR_WHITE
-	},
-	body: {
-		color: globals.COLOR_WHITE
-	},
-	button: {
+    notifShelf: {
+        overflowX: 'hidden',
+        alignItems: 'center',
+        zIndex: 2,
+        backgroundColor: globals.COLOR_WHITE,
+        height: '100%',
+        transition: '500ms',
+        borderWidth: 1,
+        borderStyle: 'none',
+    },
+    notification: {
+        backgroundColor: globals.COLOR_WHITE,
+        alignItems: 'left',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginTop: '.5em',
+        marginBottom: '.5em',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    button: {
+        width: '2em',
+        height: '2em',
+        fontSize: '1em',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: globals.COLOR_BLUE,
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: globals.COLOR_WHITE
+    },
+    notificationText: {
+        fontSize: '1em',
+        paddingTop: 0,
+        paddingLeft: '.2em',
+        paddingRight: '.2em',
+        paddingBottom: '.5em',
+        color: globals.COLOR_GRAY
+    },
+    text: {
+        fontSize: '.75em',
+        paddingTop: '.5em',
+        paddingLeft: '.2em',
+        paddingRight: '.2em',
+        fontWeight: 'bold',
+        color: globals.COLOR_GRAY
+    },
 
-	}
 });
