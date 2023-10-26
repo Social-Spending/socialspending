97059ecd4623cf9ee974ed267759cae4e923de7f
Matthew Duphily <matthew.duphily@livecom>
2023-10-26 07:33:26 +0000

    edit repo_pull.php for main

--- repo_pull.php
+++ repo_pull.php
@@ -1,8 +1,8 @@
 <?php
 
 // this should only be accessed by POST from github
-const HEADER_SIGNATURE_NAME='X-Hub-Signature-256';
-if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_POST[HEADER_SIGNATURE_NAME]))
+const HEADER_SIGNATURE_NAME='HTTP_X_HUB_SIGNATURE_256';
+if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_SERVER[HEADER_SIGNATURE_NAME]))
 {
     echo "Request must be POST with ".HEADER_SIGNATURE_NAME." in the header";
     http_response_code(400);
@@ -12,9 +12,12 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_POST[HEADER_SIGNATURE_NAME]
 // function to verify the signature, so that we know this came from github
 function verify_signature($payload)
 {
+    // shared secret with this server and github
     $secret = getenv('GITHUB_WEBHOOK_SECRET');
+    // calculate HMAC using SHA-256
     $signature = 'sha256=' . hash_hmac('sha256', $payload, $secret);
-    if ($_POST[HEADER_SIGNATURE_NAME] != $signature)
+    // compare calculated hash with expected hash included in the header
+    if ($_SERVER[HEADER_SIGNATURE_NAME] != $signature)
     {
         echo "Signatures do not match";
         http_response_code(403);
@@ -23,11 +26,14 @@ function verify_signature($payload)
 
 }
 
-$payload = json_decode(stream_get_contents(STDIN));
+// get string representation of body and verify
+$payload = file_get_contents('php://input');
 verify_signature($payload);
 
+// pull changes and override local changes
+
 echo "Pulling main";
-`git pull`;
+`git fetch && git reset --hard origin/master`;
 echo "Compiling react";
 `cd react_source && ./compile.sh`;
 echo "Pull successful";
