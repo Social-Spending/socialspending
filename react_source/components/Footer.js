3883779560603c27b3c8235ec94830daeec22d99
Ryder Reed <ryderreed15@gmail.com>
2023-10-30 13:30:52 -0400

    Fixed footer to not be terrible

--- react_source/components/Footer.js
+++ react_source/components/Footer.js
@@ -1,6 +1,7 @@
 import * as globals from '../utils/globals.js'
 
 import { StyleSheet, Text, View } from 'react-native';
+import { Link, router } from "expo-router";
 
 export default function Footer() {
     return (
@@ -8,6 +9,11 @@ export default function Footer() {
         <View style={styles.footer}>
 
             <Text style={{ color: globals.COLOR_BEIGE }}>Copyright SocialSpending© 2023</Text>
+            <View style={{flexDirection: 'row'}}>
+                <Link href="/faq" style={{color: globals.COLOR_BEIGE}}> FAQ </Link>
+                <Link href="/about" style={{color: globals.COLOR_BEIGE}}> About </Link>
+                <Link href="/contact" style={{color: globals.COLOR_BEIGE}}> Contact Us </Link>
+            </View>
 
         </View>
     );
@@ -15,7 +21,6 @@ export default function Footer() {
 
 const styles = StyleSheet.create({
     footer: {
-        position: 'absolute',
         bottom: 0,
         zIndex: 1,
         width: '100%',
@@ -23,9 +28,12 @@ const styles = StyleSheet.create({
         minHeight: '1.75em',
         backgroundColor: '#00000099',
         alignSelf: 'top',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        opacity: .8
+        opacity: .8,
+        flexDirection: 'row',
+        paddingLeft: '1em',
+        paddingRight: '1em'
     },
     
 
