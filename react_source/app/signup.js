645407d89bf20c3a6b294d4a76c5555197a23614
Ryder Reed <ryderreed15@gmail.com>
2023-11-09 19:35:53 -0500

    CFG-187 Redirect from login or signup

--- react_source/app/signup.js
+++ react_source/app/signup.js
@@ -4,11 +4,17 @@ import * as globals from '../utils/globals.js'
 import Base from '../components/Base.js';
 import Signup from '../components/Signup.js';
 import LoggedInRedirect from '../components/LoggedInRedirect.js';
+import { useLocalSearchParams } from 'expo-router';
+
+// use example.com/signup?origin={destination} to redirect to to destination after signup
 
 export default function Page() {
+
+    const { origin } = useLocalSearchParams();
+
     return (
         <Base style={globals.styles.container}>
-            <LoggedInRedirect ifLoggedIn={true} target={'/summary'}/>
+            <LoggedInRedirect ifLoggedIn={true} target={'/' + (origin ? origin : "summary")} />
             <Signup />
         </Base>
     );
