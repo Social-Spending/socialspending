b033c6178c8690d1b8bcfdbd0a4076335c926686
Matthew Duphily <matthew.duphily@live.com>
2023-12-02 19:55:09 -0500

    Styles added to 404 page

--- react_source/app/notfound.js
+++ react_source/app/notfound.js
@@ -1,34 +1,13 @@
 import * as globals from '../utils/globals.js'
-
-import { useContext } from 'react';
-import { Navigate } from "react-router-dom";
-
-import { GlobalContext } from '../components/GlobalContext.js';
-
-import "../utils/global.css"
-import Loading from '../components/Loading.js';
-
-
+import Base from '../components/Base.js';
+import Button from '../components/Button.js';
+import { Text, View, Modal } from '../utils/globals.js';
 
 export default function Page() {
 	return (
-        // <Base style={globals.styles.container} defaultDisplayNotif={true}>
-			<h1>404</h1>
-		// </Base>
+        <Base style={globals.styles.container} defaultDisplayNotif={false}>
+			<Text style={globals.styles.h1}>Page Not Found</Text>
+			{/* <Button style={globals.styles.formButton} onclick={history.back()}>Go Back</Button> */}
+		</Base>
 	);
 }
-
-const styles = {
-    summaryContainer: {
-        width: '100%',
-        flex: 1,
-        backgroundColor: globals.COLOR_BEIGE,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    summaryComponentSeparator: {
-        marginTop: '4vh'
-    }
-
-};
